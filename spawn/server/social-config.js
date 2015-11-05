2b1085966e799a04b143f9cc6003c05d8606c9d7
Joe Jacob <joejacob95@gmail.com>
2015-11-04 20:30:23 -0500

    Started FB login.

--- spawn/server/social-config.js
+++ spawn/server/social-config.js
@@ -17,3 +17,13 @@ if (Meteor.isClient) {
         }
     });
 }
+
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1650844851825940',
+    secret: '5281f547dab18a457749f5c01e44d1d9'
+});
