c4b0f8370eec9d497d7238f6edf9f10ab5c0623d
Sam Ginsburg <sbg11@duke.edu>
2015-11-05 21:58:45 -0500

    added chat feature to event pages, added some mongo collections, and updated profile data

---
+++ spawn/client/templates/addition/postComment.js
@@ -0,0 +1,20 @@
+Template.postComment.events({
+  "submit form": function (event) {
+    
+    var event_id = this._id;
+    var comment_poster = Meteor.user().profile.name;
+    var comment_message = event.target.commentMessage.value;
+    
+    EventComments.insert({
+      event_id: event_id,
+      poster: comment_poster,
+      message: comment_message
+    });
+
+    // Clear form
+    event.target.commentMessage.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+});
--- spawn/client/templates/events/viewEvent.js
+++ spawn/client/templates/events/viewEvent.js
@@ -1,5 +1,8 @@
-// Template.viewEvent.helpers({
-// 	tasks: function () {
-// 		return Tasks.find({}, {sort: {createdAt: -1}});
-// 	}
-// });
+Template.viewEvent.helpers({
+	// tasks: function () {
+	// 	return Tasks.find({}, {sort: {createdAt: -1}});
+	// }
+	comments: function () {
+		return EventComments.find({ event_id: this._id }, {sort: {createdAt: -1}});
+	}
+});
