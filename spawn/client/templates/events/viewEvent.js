2e41cb4c5a9f1c23d9bfaefd6549d2bfd74de8b1
Joe Jacob <joejacob95@gmail.com>
2015-11-18 16:30:02 -0500

    added google maps package. querying in mongo sucks

--- spawn/client/templates/events/viewEvent.js
+++ spawn/client/templates/events/viewEvent.js
@@ -4,9 +4,19 @@ Template.viewEvent.events({
 		var me = Meteor.user().username || Meteor.user().profile.name;
 		// console.log(Meteor.user().services.facebook)
 		console.log(Meteor.user());
-		Tasks.update(this._id, {
-        	$push: {attendees: {name: me, pic: Meteor.user().profile.picture, uid: Meteor.user()._id}}
-      	});
+        // unsuccesfully querying the the db lol
+       /* var i = new Meteor.Collection.ObjectID(this.id)
+        console.log(i);
+        console.log(Tasks.find({"_id" : this._id}));
+        console.log(Tasks.find( {$and: [
+                                    {"_id": this._id}, 
+                                    {"attendees.uid": Meteor.user()._id}
+                                ]}));*/
+        
+            Tasks.update(this._id, {
+                $push: {attendees: {name: me, pic: Meteor.user().profile.picture, uid: Meteor.user()._id}}
+            });
+        
 	}
 });
 
