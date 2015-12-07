bcd62d252230263e093209a67670e03ac15e1a55
Ethan Gottlieb <ethaniragottlieb@gmail.com>
2015-12-07 18:49:07 -0500

    calculated and displayed user's distance to events

--- spawn/client/templates/events/eventItem.js
+++ spawn/client/templates/events/eventItem.js
@@ -21,6 +21,13 @@ Template.eventItem.helpers({
         var eHours = (timeArr[0] > 12)? timeArr[0] -12 : timeArr[0];
         var eHours = (eHours == '00')? 12 : eHours;  
         return eHours + ":" + timeArr[1] + suffix 
+    },
+
+    getDistance: function() {
+        var latLng = Geolocation.latLng();
+        if (latLng) {
+            return getDistanceFromLatLonInKm(latLng.lat, latLng.lng, this.locationLatLng.lat, this.locationLatLng.lng);
+        }
     }
     
     /*getColor: function() {
@@ -33,6 +40,24 @@ Template.eventItem.helpers({
         return this.color;
     }*/
 });
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return parseFloat(Math.round(d * 100) / 100).toFixed(1);;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
 // should be implemented later
 /*
