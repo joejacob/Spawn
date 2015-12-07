053754a3b200cfef30ffd7f35321081aeda2d943
Ethan Gottlieb <ethaniragottlieb@gmail.com>
2015-12-07 10:13:45 -0500

    started some friend stuff

--- spawn/server/social-config.js
+++ spawn/server/social-config.js
@@ -5,9 +5,9 @@ ServiceConfiguration.configurations.remove({
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
     appId: '1650844851825940',
-    secret: '5281f547dab18a457749f5c01e44d1d9'
+    secret: '5281f547dab18a457749f5c01e44d1d9',
+    requestPermissions: ['user_friends']
 });
-
 
 Accounts.onCreateUser(function(options, user) {
     if (options.profile) {
