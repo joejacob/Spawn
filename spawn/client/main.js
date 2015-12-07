74d134c15d85305c76f4fa5ec583c09e83f19502
Greg McKeon <greg@Gregs-MacBook-Air.local>
2015-12-06 23:19:00 -0500

    fixed double loading google maps

--- spawn/client/main.js
+++ spawn/client/main.js
@@ -1,20 +1,23 @@
 if (Meteor.isClient) {
 
-    Meteor.startup(function() {
-        GoogleMaps.load();
+    Meteor.startup(function () {
+        GoogleMaps.load({
+            key: 'AIzaSyDOoNFEqag0ad113n-gkTlXU9cTvixd2Lk',
+            libraries: 'places'
+        });
     });
 
     Template.login.events({
-        'click #facebook-login': function(event) {
-            Meteor.loginWithFacebook({}, function(err){
+        'click #facebook-login': function (event) {
+            Meteor.loginWithFacebook({}, function (err) {
                 if (err) {
                     throw new Meteor.Error("Facebook login failed");
                 }
             });
         },
-     
-        'click #logout': function(event) {
-            Meteor.logout(function(err){
+
+        'click #logout': function (event) {
+            Meteor.logout(function (err) {
                 if (err) {
                     throw new Meteor.Error("Logout failed");
                 }
